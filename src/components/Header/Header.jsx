1347122ceaca15ebfb0c8e6b9fdc965620638d77
s248h176 <s248h176@gmail.com>
2022-10-21 15:40:03 -0500

    Base level

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import header from './Header.css'
+
+const Header = () => {
+    return (
+        <div className = 'header'>
+            hello
+        </div>
+    );
+};
+
+export default Header;
